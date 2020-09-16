a5a27f9ca4a88c1dc90ade94f6ac5e332ca855b2
John Tran <jptran318@gmail.com>
2020-09-15 22:57:31 -0700

    feat: package.json auto-update
    @semantic-release-bot will now update package.json

--- release.config.js
+++ release.config.js
@@ -17,6 +17,10 @@ module.exports = {
     }],
     ["@semantic-release/github", {
       "assets": "amplitude*min.js"
-    }]
+    }],
+    ["@semantic-release/git", {
+      "assets": ["package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
   ],
 }
